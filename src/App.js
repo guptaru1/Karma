6172e5111b66427c085a306ed5059e3b153390b9
Ruchika Gupta <guptaru1@msu.edu>
2024-07-27 08:09:55 -0400

    removing unused component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Header from './components/Header';
-import AboutUsSection from './components/About-Us';
+
 
 import Tabs from './components/Tabs';
 import './App.css';
